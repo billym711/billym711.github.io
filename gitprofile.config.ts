6b63aba7561ad04024febaa6e3d17d525ab9fbb0
billym711 <77858394+billym711@users.noreply.github.com>
2024-04-20 19:37:42 -0400

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -25,7 +25,7 @@ const CONFIG = {
       },
       manual: {
         // Properties for manually specifying projects
-        projects: ['billym711/finalproject'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
+        projects: ['billym711/finalproject', 'billym711/midtermapp'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
       },
     },
     external: {
